0b7f0222e826fbd8d3835dfc2f64d686db5f1128
TNamdarian <teemanamdarian@gmail.com>
2021-08-07 14:44:55 +0000

    Remove active class to restart delayed effect

--- assets/script.js
+++ assets/script.js
@@ -7,11 +7,31 @@ let navLinks = Array.from(document.querySelectorAll('.nav-link'));
 let contact = document.querySelector('.contact');
 let logo = document.querySelector('.logo');
 
-//  ..................... Creat event listener 
+//  ..................... Creat eventlistener 
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('active');
     nav.classList.toggle('active');
     navRight.classList.toggle('active');
     navLeft.classList.toggle('active');
     logo.classList.toggle('active');
+
+    // Remove active class to restart delayed effect
+    contact.classList.remove('active');
+    navRight.classList.remove('active');
+
+    if (menuToggle.classList.contains('active')) {
+        setTimeout(() => {
+            navRight.classList.add('active');
+        }, 100)
+
+        for (let i = 0; i < navLinks.length; i++) {
+            navLinks[i].classList.remove('active');
+            setTimeout(() => {
+                navLinks[i].classList.add('active')
+            }, i * 100)
+        }
+        setTimeout(() => {
+            contact.classList.add('active');
+        }, 700);
+    }
 })
