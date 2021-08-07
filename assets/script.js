1d27e8385fa1eceac070e3f5904c6e36d96bcfb0
TNamdarian <teemanamdarian@gmail.com>
2021-08-07 13:47:16 +0000

    Change the color of the menu-toggle.active

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// ..................... Create the selectores
+let menuToggle = document.querySelector('.menu-toggle');
+let nav = document.querySelector('.nav');
+let navLeft = document.querySelector('.nav-left');
+let navRight = document.querySelector('.nav-right');
+let navLinks = Array.from(document.querySelectorAll('.nav-link'));
+let contact = document.querySelector('.contact');
+let logo = document.querySelector('.logo');
+
+//  ..................... Creat event listener 
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+})
