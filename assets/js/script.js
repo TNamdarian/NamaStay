81791052670d47dcc52b5753e6a69d77ceb8b4e1
TNamdarian <teemanamdarian@gmail.com>
2021-09-06 12:59:05 +0000

    tidy up pages and delete footer and styping options

--- assets/js/script.js
+++ assets/js/script.js
@@ -174,3 +174,21 @@ function initMap() {
         }
     }
 }
+
+// ..................... Email functionality 
+function sendMail(contactForm) {
+    emailjs.send("gmail", "Teema", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.email.value,
+            "message": contactForm.message.value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+            },
+            function(error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
+}
