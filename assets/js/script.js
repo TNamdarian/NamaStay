94482612745fc5eb32604bc9985b90b9fcf95a5d
TNamdarian <teemanamdarian@gmail.com>
2021-09-07 14:06:50 +0000

    Add else to the if statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -208,6 +208,8 @@ counters.forEach(counter => {
         if (c < target) {
             counter.innerText = `${Math.ceil(c+increment)}`
             setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target
         }
     }
     updateCounter()
