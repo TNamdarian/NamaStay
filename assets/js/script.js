a13077f1f0280c6a25dbdf18b977f0522e42b3ab
TNamdarian <teemanamdarian@gmail.com>
2021-09-07 14:25:33 +0000

    Add footer to all pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -203,11 +203,11 @@ counters.forEach(counter => {
         const target = +counter.getAttribute('data-target')
         const c = +counter.innerText
 
-        const increment = target / 200
+        const increment = target / 100
 
         if (c < target) {
             counter.innerText = `${Math.ceil(c+increment)}`
-            setTimeout(updateCounter, 1)
+            setTimeout(updateCounter, 10)
         } else {
             counter.innerText = target
         }
